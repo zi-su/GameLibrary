ba439dfb4ad4a31b6e658fa50c16d60bca33a704
Masato Naito <aitona765@gmail.com>
2020-02-11 12:26:53 +0900

    no message

--- Assets/GameLibrary/Script/UI/ScrollMover.cs
+++ Assets/GameLibrary/Script/UI/ScrollMover.cs
@@ -7,6 +7,8 @@ namespace GameLibrary{
 	public class ScrollMover : MonoBehaviour{
         [SerializeField] ScrollRect scrollRect;
         [SerializeField] VerticalLayoutGroup verticalLayoutGroup;
+        [SerializeField] HorizontalLayoutGroup horizontalLayoutGroup;
+
         RectTransform content;
         RectTransform scrollTrans;
         int index = 0;
@@ -27,14 +29,34 @@ namespace GameLibrary{
             {
                 MoveUp();
             }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveRight();
+            }
         }
         
-        bool OutArea(int index)
+        bool OutAreaVertical(int index)
         {
             var child = content.GetChild(index) as RectTransform;
             float top = child.localPosition.y + child.sizeDelta.y * child.pivot.y + content.localPosition.y;
             float bottom = child.localPosition.y - child.sizeDelta.y * child.pivot.y + content.localPosition.y;
             if(top > 0 || Mathf.Abs(bottom) > scrollTrans.sizeDelta.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool OutAreaHorizontal(int index)
+        {
+            var child = content.GetChild(index) as RectTransform;
+            float left = child.localPosition.x - child.sizeDelta.x * child.pivot.x + content.localPosition.x;
+            float right = child.localPosition.x + child.sizeDelta.x * child.pivot.x + content.localPosition.x;
+            if (left < 0 || right > scrollTrans.sizeDelta.x)
             {
                 return true;
             }
@@ -51,7 +73,7 @@ namespace GameLibrary{
                     index = 0;
                 }
             }
-            if (OutArea(index))
+            if (OutAreaVertical(index))
             {
                 //現在位置、次の位置の差分だけ移動
                 var child = content.GetChild(index) as RectTransform;
@@ -74,7 +96,7 @@ namespace GameLibrary{
                     index = content.childCount - 1;
                 }
             }
-            if (OutArea(index))
+            if (OutAreaVertical(index))
             {
                 //現在位置、次の位置の差分だけ移動
                 var child = content.GetChild(index) as RectTransform;
@@ -84,6 +106,51 @@ namespace GameLibrary{
                 if (index == 0) n = 1.0f;
                 n = Mathf.Clamp01(n);
                 DOTween.To(() => scrollRect.verticalNormalizedPosition, x => scrollRect.verticalNormalizedPosition = x, n, 0.2f);
+            }
+        }
+
+        public void MoveLeft()
+        {
+            var now = content.GetChild(index) as RectTransform;
+            {
+                index--;
+                if (index < 0)
+                {
+                    index = content.childCount - 1;
+                }
+            }
+            if (OutAreaHorizontal(index))
+            {
+                //現在位置、次の位置の差分だけ移動
+                var child = content.GetChild(index) as RectTransform;
+                float diff = child.localPosition.x - now.localPosition.x;
+                float ndiff = diff / (content.sizeDelta.x - scrollTrans.sizeDelta.x);
+                float n = scrollRect.horizontalNormalizedPosition + ndiff;
+                if (index == 0) n = 0.0f;
+                n = Mathf.Clamp01(n);
+                DOTween.To(() => scrollRect.horizontalNormalizedPosition, x => scrollRect.horizontalNormalizedPosition = x, n, 0.2f);
+            }
+        }
+
+        public void MoveRight()
+        {
+            var now = content.GetChild(index) as RectTransform;
+            {
+                index++;
+                if (index > content.childCount - 1)
+                {
+                    index = 0;
+                }
+            }
+            if (OutAreaHorizontal(index))
+            {
+                //現在位置、次の位置の差分だけ移動
+                var child = content.GetChild(index) as RectTransform;
+                float diff = child.localPosition.x - now.localPosition.x;
+                float ndiff = diff / (content.sizeDelta.x - scrollTrans.sizeDelta.x);
+                float n = scrollRect.horizontalNormalizedPosition + ndiff;
+                n = Mathf.Clamp01(n);
+                DOTween.To(() => scrollRect.horizontalNormalizedPosition, x => scrollRect.horizontalNormalizedPosition = x, n, 0.2f);
             }
         }
 	}
